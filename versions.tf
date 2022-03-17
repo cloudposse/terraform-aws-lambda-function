3a184d0e440086c2a38b06ce565fab06e3febe75
Matt Calhoun <matt@mattcalhoun.com>
2022-03-17 10:56:24 -0400

    fix provider constraints

--- versions.tf
+++ versions.tf
@@ -2,10 +2,9 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    # Update these to reflect the actual requirements of your module
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
     }
   }
 }
