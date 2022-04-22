974e3bafbfc2597f7323d5a8805a7594f5b89faf
nitrocode <nitrocode@users.noreply.github.com>
2022-04-22 08:54:57 -0500

    More robust no changes check

--- test/src/common.go
+++ test/src/common.go
@@ -31,6 +31,7 @@ func testNoChanges(t *testing.T, terraformDir string) {
 
 	terraform.Init(t, terraformOptions)
 	plan := terraform.Plan(t, terraformOptions)
+	planContainsNoChanges := strings.Contains(plan, "No changes.") || strings.Contains(plan, "0 to add, 0 to change, 0 to destroy.")
 
-	assert.Contains(t, plan, "No changes.")
+	assert.True(t, planContainsNoChanges)
 }
